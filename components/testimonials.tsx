0e996842023d54f82d49431e667a508141f18984
galifornia <martinho87@gmail.com>
2022-04-19 13:29:11 +0200

    Rearrange TrustedBy layout

---
+++ components/testimonials.tsx
@@ -0,0 +1,7 @@
+type Props = {};
+
+const Testimonials: React.FC<Props> = () => {
+  return <div>Testimonials</div>;
+};
+
+export default Testimonials;
--- components/trusted_by.tsx
+++ components/trusted_by.tsx
@@ -1,30 +1,38 @@
 import ClientLogos from './client-logos';
+import Testimonials from './testimonials';
 
 interface Props {}
 
 const TrustedBy: React.FC<Props> = () => {
   return (
-    <section className='bg-white min-h-screen flex flex-col justify-center gap-16 md:gap-32'>
-      <div className='flex flex-col justify-center items-center'>
-        <h3 className='text-xl mb-10 mt-16 md:mt-32 font-bold text-center'>
-          <span className='whitespace-nowrap'>Trusted by </span>
-          <span className='whitespace-nowrap'>
-            some of the apps you use daily
-          </span>
-        </h3>
-        <ClientLogos />
+    <>
+      <section className='bg-white min-h-screen flex flex-col justify-center gap-16 md:gap-32'>
         <div className='flex flex-col justify-center items-center'>
-          <div className='container mx-auto lg:max-w-[70%] lg:px-10'>
-            <h3 className='mt-16 text-3xl lg:text-4xl tracking-tight text-center px-10 !leading-[3.5rem]'>
-              We believe in good communication and a fully transparent
-              development process.
-            </h3>
+          <h3 className='text-xl mb-10 mt-16 md:mt-32 font-bold text-center'>
+            <span className='whitespace-nowrap'>Trusted by </span>
+            <span className='whitespace-nowrap'>
+              some of the apps you use daily
+            </span>
+          </h3>
+          <ClientLogos />
+          <div className='flex flex-col justify-center items-center'>
+            <div className='container mx-auto lg:max-w-[70%] lg:px-10'>
+              <h3 className='mt-16 text-3xl lg:text-4xl tracking-tight text-center px-10 !leading-[3.5rem]'>
+                We believe in good communication and a fully transparent
+                development process.
+              </h3>
+            </div>
           </div>
         </div>
-      </div>
-      <div>Testimonials</div>
-      <div className='flex-1 bg-black'></div>
-    </section>
+      </section>
+
+      <section>
+        <div>Testimonials</div>
+        <div className='flex-1 bg-black text-white'>
+          <Testimonials />
+        </div>
+      </section>
+    </>
   );
 };
 
