c2bfedfa9241a64406bde2cbc7da7fcb9f9d6eaf
galifornia <martinho87@gmail.com>
2022-04-19 17:34:00 +0200

    MastHead: fix logo size

--- components/masthead.tsx
+++ components/masthead.tsx
@@ -1,6 +1,6 @@
-import Image from "next/image";
-import { useCallback, useContext, useRef, useState } from "react";
-import { ScrollContext } from "../utils/scroll-observer";
+import Image from 'next/image';
+import { useCallback, useContext, useRef, useState } from 'react';
+import { ScrollContext } from '../utils/scroll-observer';
 
 type Props = {};
 
@@ -26,43 +26,43 @@ const MastHead: React.FC = ({}: Props) => {
       style={{
         transform: `translateY(-${progress * 20}vh)`,
       }}
-      className="min-h-screen flex flex-col items-center justify-center text-white sticky top-0 -z-10"
+      className='min-h-screen flex flex-col items-center justify-center text-white sticky top-0 -z-10'
     >
       <video
         autoPlay
         muted
         playsInline
         loop
-        className="absolute w-full h-full object-cover"
+        className='absolute w-full h-full object-cover'
       >
-        <source src="/assets/masthead-bg.mp4" type="video/mp4" />
+        <source src='/assets/masthead-bg.mp4' type='video/mp4' />
         {/* <source src='/assets/masthead_bg.webm' type='video/webm; codecs=vp9' /> */}
       </video>
       <div className={`flex-grow-0 pt-10 transition-opacity duration-1000`}>
         <Image
-          src="/assets/logo.svg"
-          width={128 / 1.5}
-          height={114 / 1.5}
-          alt="agency logo"
+          src='/assets/logo.svg'
+          width={128 / 3}
+          height={114 / 3}
+          alt='agency logo'
         />
       </div>
-      <div className="p12 font-bold text-white z-10 drop-shadow-[0_5px_3px_rgba(0,0,0,0.4)] text-center flex-1 flex flex-col items-center justify-center">
-        <h1 className="mb-6 text-4xl xl:text-5xl">Beatitudo</h1>
-        <h2 className="mb-2 text-2xl xl:text-3xl tracking-tight">
+      <div className='p12 font-bold text-white z-10 drop-shadow-[0_5px_3px_rgba(0,0,0,0.4)] text-center flex-1 flex flex-col items-center justify-center'>
+        <h1 className='mb-6 text-4xl xl:text-5xl'>Beatitudo</h1>
+        <h2 className='mb-2 text-2xl xl:text-3xl tracking-tight'>
           <span>App Development,</span> <span>done right.</span>
         </h2>
       </div>
 
       <div
         className={`flex flex-grow-0 pb-20 md:pd-10 transition-all duration-1000 ${
-          imageLoaded ? "opacity-100" : "opacity-0 -translate-y-10"
+          imageLoaded ? 'opacity-100' : 'opacity-0 -translate-y-10'
         }`}
       >
         <Image
-          src="/assets/arrow-down.svg"
+          src='/assets/arrow-down.svg'
           width={188 / 4}
           height={105 / 4}
-          alt="Scroll down arrow"
+          alt='Scroll down arrow'
           onLoadingComplete={handleImageLoaded}
         ></Image>
       </div>
